37c0f1422aee0c86db1fb93913049393ed29ec50
LCGC-04406-205\dell <majmgg@gmail.com>
2017-04-06 18:00:51 +0800

    Test

---
+++ LN.API2/Controllers/HotelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LN.API2.Models;
+
+namespace LN.API2.Controllers
+{
+    public class HotelController : ApiController
+    {
+        Hotel[] tempHotels = new Hotel[]
+        {
+            new Hotel { Name="酒店01", Address="厦门01" },
+            new Hotel { Name="酒店02", Address="厦门02" },
+            new Hotel { Name="酒店03", Address="厦门03" }
+        };
+        [HttpGet]
+        public Hotel GetHotels(string Name)
+        {
+            return tempHotels.FirstOrDefault(m => m.Name == Name);
+        }
+
+        [HttpPost]
+        public IEnumerable<Hotel> AllHotels()
+        {
+            return tempHotels;
+        }
+    }
+}
---
+++ LN.API2/Models/Hotel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LN.API2.Models
+{
+    public class Hotel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
